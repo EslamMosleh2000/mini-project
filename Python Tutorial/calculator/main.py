35629a13ad0ae900bb9a89dd4fdbd9764e348451
Eslam <you@example.com>
2024-12-29 02:54:05 +0200

    calculator Project

---
+++ Python Tutorial/calculator/main.py
@@ -0,0 +1,11 @@
+first_number = input("enter the first number please")
+second_number = input("enter the second number please")
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print(sum)
+print(subtraction)
+print(multiplication)
+print(division)
